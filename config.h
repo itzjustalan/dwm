89292855a61b722efd9db099771b1a107b2c5504
devalan <devtesting1212@gmail.com>
2021-01-04 14:25:04 +0530

    drac cyan

--- config.h
+++ config.h
@@ -6,21 +6,23 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+//static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "jetbrainsmono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan_og[]        = "#005577";
+static const char col_cyan_drac[]        = "#8be9fd";
 static const char col_cyan[]        = "#21222c";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan_og  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan_drac},
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "f", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
