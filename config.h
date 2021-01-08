116eddd912da661cf2cff01cc6892c25f6cf20cf
devalan <devtesting1212@gmail.com>
2021-01-09 05:13:05 +0530

    swallow

--- config.h
+++ config.h
@@ -29,17 +29,18 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "f", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "-", "5", "6", "📻", "🌏", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "float",    NULL,       "notes",    0,            1,           -1 },
+	/* class      instance    title       tags mask    isterminal  noswallow  isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,           0,            0,       1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,      0,            -1,      0,           -1 },
+	{ "st",       NULL,       NULL,       1 << 8,      1,            1,       1,           -1 },
+	{ "float",    NULL,       "notes",    0,           0,            0,       1,           -1 },
 };
 
 /* layout(s) */
