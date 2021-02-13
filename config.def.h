327e438bb0250192bdf56b7ba283fb46bd9a316f
devalan <devtesting1212@gmail.com>
2021-02-13 14:03:17 +0530

    horizontal grid

--- config.def.h
+++ config.def.h
@@ -34,18 +34,26 @@ static const Rule rules[] = {
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
+<<<<<<<
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 #include "layouts.c"
+=======
+#include "horizgrid.c"
+>>>>>>>
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+<<<<<<<
 	{ "HHH",      grid },
+=======
+	{ "###",      horizgrid },
+>>>>>>>
 };
 
 /* key definitions */
